819fc47da7bea43b3f3fb622a1ecaadcf9aa96dc
tvandinther <tvandinther@gmail.com>
2020-11-25 19:44:08 +1300

    EOD commit

--- GameOfLife.Tests/GridTest.cs
+++ GameOfLife.Tests/GridTest.cs
@@ -1,10 +1,47 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace GameOfLife.Tests
 {
     public class GridTest
     {
+        [Theory]
+        [InlineData(10, 10)]
+        public void Grid_ShouldInstantiate(int width, int height)
+        {
+            var grid = new Grid(width, height);
+            Assert.IsType<Grid>(grid);
+        }
+        
+        [Theory]
+        [InlineData(2, 2, null)]
+        public void Grid_ShouldInstantiate_WithState(int width, int height, bool[][] state)
+        {
+            var grid = new Grid(width, height, state);
+            Assert.IsType<Grid>(grid);
+        }
+        
+        [Theory]
+        [InlineData(-10, 10)]
+        [InlineData(10, -10)]
+        [InlineData(-10, -10)]
+        [InlineData(0, 10)]
+        [InlineData(10, 0)]
+        [InlineData(0, 0)]
+        public void Grid_InvalidDimensions_ShouldThrowException(int width, int height)
+        {
+            Assert.Throws<Exception>(() => new Grid(width, height));
+        }
+        
+        [Theory]
+        [InlineData(2, 2, new bool[][] {{false, false, false}, {false, false, false}, {false, false, false}})]
+        public void Grid_InvalidState_ShouldThrowException(int width, int height, bool[][] state)
+        {
+            Assert.Throws<Exception>(() => new Grid(width, height, state));
+        }
+        
+        
         [Theory]
         [InlineData(10, 10)]
         [InlineData(15, 20)]
@@ -14,18 +51,6 @@ namespace GameOfLife.Tests
             var grid = new Grid(expectedWidth, expectedHeight);
             Assert.Equal(expectedHeight, grid.Height);
             Assert.Equal(expectedWidth, grid.Width);
-        }
-
-        [Theory]
-        [InlineData(-10, 10)]
-        [InlineData(10, -10)]
-        [InlineData(-10, -10)]
-        [InlineData(0, 10)]
-        [InlineData(10, 0)]
-        [InlineData(0, 0)]
-        public void Grid_NegativeDimension_ShouldThrowException(int width, int height)
-        {
-            Assert.Throws<Exception>(() => new Grid(width, height));
         }
     }
 }
